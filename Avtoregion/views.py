eb3706ead80d628b3d6bfdba30765b2dd1d3e8be
rquid <p3r1d0z@gmail.com>
2017-12-19 17:29:52 +0500

    .

--- Avtoregion/views.py
+++ Avtoregion/views.py
@@ -280,7 +280,7 @@ def AccumulateSup(req):
         fields = [field.name for field in Race._meta.fields]
         fields.remove('weight_unload')
         qResponse = Race.objects.filter(supplier__inn__exact=req.POST.get('radio' or None),
-                                        race_date__range=[req.POST.get('from'), req.POST.get('to')]).values(*fields)
+                                   race_date__range=[req.POST.get('from'), req.POST.get('to')]).values(*fields)
         print(qResponse)
         return render(request=req, template_name='Avtoregion/account.html', context={'qResponce': qResponse})
 
@@ -292,8 +292,7 @@ def AccumulateCus(req):
     if req.method == 'POST':
         fields = [field.name for field in Race._meta.fields]
         fields.remove('weight_load')
-        print(fields)
-        qResponse = Race.objects.filter(*fields, customer__inn__exact=req.POST.get('radio' or None),
-                                        race_date__range=[req.POST.get('from'), req.POST.get('to')])
+        qResponse = Race.objects.filter(customer__inn__exact=req.POST.get('radio' or None),
+                                        race_date__range=[req.POST.get('from'), req.POST.get('to')]).select_related(*fields)
         return render(request=req, template_name='Avtoregion/account.html', context={'qResponce': qResponse})
 
