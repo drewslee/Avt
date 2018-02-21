36cb8cfdb33820a8441b33c40fc18eb31671688a
rquid <p3r1d0z@gmail.com>
2018-02-21 11:58:42 +0500

    .

--- static/js/main.js
+++ static/js/main.js
@@ -98,89 +98,10 @@ $(function () {
     $('input[name="daterange"]').on('apply.daterangepicker', function (ev, picker) {
         $('form[id="push_date"]').submit()
     })
-});
 
 $(document).on('click', '.delete-confirmation', function () {
     return confirm('Вы уверены, что хотите удалить?');
 });
-
-$(function () {
-    $('input[name="arrival_time"]').daterangepicker({
-        "singleDatePicker": true,
-        "autoApply": true,
-        "timePicker": true,
-        "timePicker24Hour": true,
-        "startDate": new Date(),
-        "endDate" : new Date(),
-        locale: {
-            "monthNames": [
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"
-            ],
-            format: 'YYYY-MM-DD H:mm',
-            "firstDay": 1,
-            "weekLabel": "Неделя",
-            "daysOfWeek": [
-                "Вс",
-                "Пн",
-                "Вт",
-                "Ср",
-                "Чт",
-                "Пт",
-                "Сб"
-            ]
-
-        }
-    });
-});
-$(function () {
-    $('input[name="race_date"]').daterangepicker({
-        "singleDatePicker": true,
-        "autoApply": true,
-        "timePicker": true,
-        "timePicker24Hour": true,
-        "startDate": new Date(),
-        "endDate" : new Date(),
-        locale: {
-            "monthNames": [
-                "Январь",
-                "Февраль",
-                "Март",
-                "Апрель",
-                "Май",
-                "Июнь",
-                "Июль",
-                "Август",
-                "Сентябрь",
-                "Октябрь",
-                "Ноябрь",
-                "Декабрь"
-            ],
-            format: 'YYYY-MM-DD H:mm',
-            "firstDay": 1,
-            "weekLabel": "Неделя",
-            "daysOfWeek": [
-                "Вс",
-                "Пн",
-                "Вт",
-                "Ср",
-                "Чт",
-                "Пт",
-                "Сб"
-            ]
-
-        }
-    });
 
     $('#race_table').bootstrapTable({
         showColumns: true,
