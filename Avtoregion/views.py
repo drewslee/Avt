74014e3c1463af0e560a380ab67b5ad56e327339
rquid <p3r1d0z@gmail.com>
2018-07-04 13:58:37 +0500

    fix Mediator form

--- Avtoregion/views.py
+++ Avtoregion/views.py
@@ -289,7 +289,7 @@ class MediatorViewList(LoginRequiredMixin, AliveListViewMixin, ListView):
 
     def get_context_data(self, object_list=None, **kwargs):
         kwargs = super().get_context_data(**kwargs)
-        kwargs['form'] = SupplierForm()
+        kwargs['form'] = MediatorForm()
         return kwargs
 
 
